83b43854209f2a9c19236d364a5eb4ead895c8db
Atrox <saif.ahmed1215@gmail.com>
2024-06-25 11:17:31 +0300

    finalised all errors and added animation for the mobile menu

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { Socials } from "@/constants";
+import { slideInFromTop } from "@/utils/motion";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import React, { useState } from "react";
 import { FaBars } from "react-icons/fa";
@@ -9,8 +11,8 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="w-full h-[65px] fixed top-0  z-50 ">
-      <div className="w-full h-full flex flex-row items-center  background-blur  justify-around m-auto px-[10px]  ">
+    <div className="w-full h-[65px] relative top-0  z-50 ">
+      <div className="w-full h-full flex absolute flex-row items-center z-50 background-blur border-none justify-around m-auto px-[10px]  ">
         <a
           href="#about-me"
           className="h-auto w-auto flex flex-row items-center"
@@ -20,7 +22,7 @@ const Navbar = () => {
             alt="logo"
             width={70}
             height={70}
-            className="cursor-pointer hover:animate-slowspin"
+            className="cursor-pointer hover:animate-slowspin z-50 "
           />
 
           <span className="font-bold ml-[10px] hidden md:block text-gray-300">
@@ -67,40 +69,48 @@ const Navbar = () => {
         </button>
       </div>
       {/* Mobile Menu */}
-      {isOpen && (
-        <div className="absolute top-16 left-0 right-0 md:hidden z-50 h-screen  background-blur text-[20px] border-none text-center  rounded-lg">
-          <div className="flex flex-col pt-14 gap-10 space-y-3 py-3 px-4 rounded-md h-full font-bold text-gray-200   ">
-            <a
-              href="#about-me"
-              className="block py-2 transition duration-300 ease-in-out hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-500 hover:to-cyan-500"
-              onClick={() => setIsOpen(false)}
-            >
-              About me
-            </a>
-            <a
-              href="#skills"
-              className="block py-2 transition duration-300 ease-in-out hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-500 hover:to-cyan-500"
-              onClick={() => setIsOpen(false)}
-            >
-              Skills
-            </a>
-            <a
-              href="#projects"
-              className="block py-2 transition duration-300 ease-in-out hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-500 hover:to-cyan-500"
-              onClick={() => setIsOpen(false)}
-            >
-              Projects
-            </a>
-            <a
-              href="#contact"
-              className="block py-2 transition duration-300 ease-in-out hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-500 hover:to-cyan-500"
-              onClick={() => setIsOpen(false)}
-            >
-              Contact
-            </a>
-          </div>
+      <motion.div
+        initial={{ opacity: 0, x: 100 }}
+        animate={isOpen ? { opacity: 1, x: 0 } : { opacity: 0, x: 100 }}
+        className={`${
+          isOpen
+            ? "absolute top-[65px] left-0 right-0 md:hidden z-40 h-screen background-blur  text-[20px] border-none text-center"
+            : "hidden"
+        }`}
+      >
+        <div className="flex flex-col pt-14 gap-10 space-y-3 py-3 px-4 rounded-md h-full font-bold text-gray-200   ">
+          <a
+            href="#about-me"
+            className="block py-2 transition duration-300 ease-in-out hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-500 hover:to-cyan-500"
+            onClick={() => setIsOpen(false)}
+          >
+            About me
+          </a>
+          <a
+            href="#skills"
+            className="block py-2 transition duration-300 ease-in-out hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-500 hover:to-cyan-500"
+            onClick={() => setIsOpen(false)}
+          >
+            Skills
+          </a>
+          <a
+            href="#projects"
+            className="block py-2 transition duration-300 ease-in-out hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-500 hover:to-cyan-500"
+            onClick={() => setIsOpen(false)}
+          >
+            Projects
+          </a>
+          <a
+            href="#contact"
+            className="block py-2 transition duration-300 ease-in-out hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-500 hover:to-cyan-500"
+            onClick={() => setIsOpen(false)}
+          >
+            Contact
+          </a>
         </div>
-      )}
+      </motion.div>
+      {/* {isOpen && (
+      )} */}
     </div>
   );
 };
