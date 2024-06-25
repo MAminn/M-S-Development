a9662297b00d72425165ee5d6770ab3ad9d617eb
Atrox <saif.ahmed1215@gmail.com>
2024-06-25 10:21:48 +0300

    trial error 4

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <div className="w-full h-[65px] fixed top-0  z-50 ">
-      <div className="w-full h-full flex flex-row items-center bg-[#03001456] background-blur  justify-around m-auto px-[10px]  ">
+      <div className="w-full h-full flex flex-row items-center  background-blur  justify-around m-auto px-[10px]  ">
         <a
           href="#about-me"
           className="h-auto w-auto flex flex-row items-center"
@@ -68,7 +68,7 @@ const Navbar = () => {
       </div>
       {/* Mobile Menu */}
       {isOpen && (
-        <div className="absolute top-16 left-0 right-0 md:hidden z-50 h-screen bg-[#03001456] background-blur text-[20px] border-none text-center  rounded-lg">
+        <div className="absolute top-16 left-0 right-0 md:hidden z-50 h-screen  background-blur text-[20px] border-none text-center  rounded-lg">
           <div className="flex flex-col pt-14 gap-10 space-y-3 py-3 px-4 rounded-md h-full font-bold text-gray-200   ">
             <a
               href="#about-me"
