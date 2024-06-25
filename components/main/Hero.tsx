9b0e94a39ccf751d5c79c34ee464283e9a4419c2
Atrox <saif.ahmed1215@gmail.com>
2024-06-25 12:35:04 +0300

    tweaked somethings and finalised what need to be adjusted

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -4,14 +4,11 @@ import HeroContent from "../sub/HeroContent";
 const Hero = () => {
   return (
     <section className=" relative flex flex-col h-full w-full" id="about-me">
-      <video
-        autoPlay
-        muted
-        loop
-        className=" rotate-180 absolute top-[-340px] left-0 -z-10 w-full h-full object-cover  cursor-none "
-      >
-        <source src="/blackhole.webm" type="video/webm" />
-      </video>
+      <div className="rotate-180 absolute top-[-330px] max-md:top-[-280px] left-0  w-full h-full object-cover  cursor-none -z-50">
+        <video autoPlay muted loop className="w-full h-full  ">
+          <source src="/blackhole.webm" type="video/webm" />
+        </video>
+      </div>
       <div className="absolute top-0 left-0 w-full h-full bg-transparent z-[5]"></div>
       <HeroContent />
     </section>
